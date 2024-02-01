a75bd10602cf306cda2ffa1138d3f9f4866d594c
Aadityanshu Singh <aadityanshu.singh.che22@itbhu.ac.in>
2024-02-01 20:35:14 +0530

    Wrote Logic for Login and Signup UI

--- client/ssfe/src/App.jsx
+++ client/ssfe/src/App.jsx
@@ -9,6 +9,7 @@ import { ResetPassword } from './pages/ResetPassword';
 import { ChooseNewPassword } from './pages/ChooseNewPassword';
 import { ResetComplete } from './pages/ResetComplete';
 import { Dashborad } from './pages/Dashborad';
+import { SignUp } from './pages/SignUp';
 
 function App() {
 
@@ -19,6 +20,7 @@ function App() {
       <Routes>
         <Route index element={<Login/>}/>
         <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
         <Route path='/otp' element={<OTP/>}/>
         <Route path='/resetpassword' element={<ResetPassword/>}/>
         <Route path='/choosenewpassword' element={<ChooseNewPassword/>}/>
