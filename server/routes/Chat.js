dc10dab2af6a412edba64d01a04375dd3d92dd47
Aadityanshu Singh <aadityanshu.singh.che22@itbhu.ac.in>
2024-04-08 00:10:39 +0530

    corrected the chat path

--- server/routes/Chat.js
+++ server/routes/Chat.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 // Import the required controllers and middleware functions
-const { updateChat, getChats } = require("../../server/controllers/Chat");
+const { updateChat, getChats } = require("../../server/controllers/chat");
 
 const { auth } = require("../middlewares/auth");
 
