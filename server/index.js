0579d975a66f68afa91d267b5d0ded09ad48d23b
Aadityanshu Singh <aadityanshu.singh.che22@itbhu.ac.in>
2024-04-08 04:59:37 +0530

    Again updated some cors related stuff

--- server/index.js
+++ server/index.js
@@ -53,12 +53,20 @@ const cloudinary = require("./config/cloudinary");
 cloudinary.cloudinaryConnect();
 
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "https://socio-sync.vercel.app/",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "https://socio-sync.vercel.app/",
+//     credentials: true,
+//   })
+// );
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://socio-sync.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
 
 // Mounting Of Routes
 app.use("/api/v1/auth", userRoutes);
