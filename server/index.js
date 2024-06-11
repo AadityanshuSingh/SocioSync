f044c517b44e83ef403f8fc512412a5dc8d7845c
Aadityanshu Singh <aadityanshu.singh.che22@itbhu.ac.in>
2024-06-11 16:34:25 +0530

    Added the missing line for creating http server in index.js

--- server/index.js
+++ server/index.js
@@ -22,6 +22,7 @@ const { handleSocketConnections } = require("./controllers/SocketController");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 
+const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
     origin: "https://socio-sync.vercel.app",
